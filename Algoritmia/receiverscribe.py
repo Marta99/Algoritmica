4c3753f4d79bc95697e0aeb7f6448d8c768f0ef4
Oriol <oriolalascercos@gmail.com>
2019-04-07 21:13:16 +0200

    Fet mitat recursius

--- Algoritmia/receiverscribe.py
+++ Algoritmia/receiverscribe.py
@@ -77,6 +77,63 @@ def main():
         else:
             print('KO', file=outputfile)
 
+def sumatori_vector_r(vector):
+    """ Suma tots els valors del vector i retorna el seu resultat"""
+    if vector is []:
+        return 0
+    return ord(vector[0]) + sumatori_vector_r(vector[1:])
+
+def llegir_resultat_suma_r(inputfile, byte, sumatori):
+    """ Llegeix un dels sumatoris del text"""
+    if not byte or byte == ' ':
+        return int(''.join(sumatori))
+    sumatori.append(byte)
+    return llegir_resultat_suma_r(inputfile, inputfile.read(1), sumatori)
+
+def main_r():
+    """Reads from stdin and outputs to stdout the same sequence of bytes and checks the hash byte"""
+    inputfile = sys.stdin
+    outputfile = sys.stdout
+
+    vector = []
+    byte = inputfile.read(1)
+    byte2 = inputfile.read(1)
+    posicio = 2
+    while byte != ' ' or byte2 != ' ':
+        vector.append(byte)
+        byte = byte2
+        posicio += 1
+        byte2 = inputfile.read(1)
+    suma_text = sumatori_vector(vector)
+    byte = inputfile.read(1)
+    suma_checker_text = 0
+    posicio_vector = 0
+    trobada_diferencia = False
+
+    # Calcules la suma del checker
+    while byte != ' ':
+        caracter = (ord(byte) - ord('0'))
+        if caracter != ord(vector[posicio_vector]) % 10:
+            posicio_errata = posicio_vector
+            num_caracter_error = caracter
+            trobada_diferencia = True
+        suma_checker_text += caracter
+        byte = inputfile.read(1)
+        posicio_vector += 1
+
+    valor_primer_sumatori = llegir_resultat_suma(inputfile, inputfile.read(1), [])
+    valor_segon_sumatori = llegir_resultat_suma(inputfile, inputfile.read(1), [])
+
+    if not trobada_diferencia and suma_text == suma_checker_text and\
+            valor_primer_sumatori == suma_checker_text and\
+            valor_segon_sumatori == suma_checker_text:
+        print('OK', file=outputfile)
+    else:
+        if trobada_diferencia and valor_primer_sumatori == suma_checker_text:
+            print('KO', file=outputfile)
+            print(str(posicio_errata) + ' ' + chr(num_caracter_error + ord('0')), file=outputfile)
+        else:
+            print('KO', file=outputfile)
 
 if __name__ == "__main__":
     main()
